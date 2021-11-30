b9d5f08f7da885d4f1e78f10d25cacb1d9490566
dev420 <sspike191919@gmail.com>
2021-11-30 19:36:54 +0300

    complete header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,51 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
 
-//BEM -> block Element Modifier
-
 const Navbar = () => {
-    return (
-        <div className="victory__navbar">            
-            <div className="victory__navbar-links">
-                <div className="vitory__nabvar-links_logo">
-                    <img src={logo} alt="logo"></img>
-                </div>
-                <div className="victory__navbar-links_container">
-                    <p><a href="#home">Home</a></p>
-                    <p><a href="#victory">Victory</a></p>
-                    <p><a href="#possibility">OepnAI</a></p>
-                    <p><a href="#features">Case Studies</a></p>
-                    <p><a href="#blog">Library</a></p>                    
-                </div>
-                <div className="victory__navbar-sign">
-                    <p>Sign in</p>
-                    <button type="button">Sign up</button>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-export default Navbar
+  return (
+    <div className="victory__navbar">
+      <div className="victory__navbar-links">
+        <div className="victory__navbar-links_logo">
+        <img src={logo} alt="logo"></img>
+        </div>
+        <div className="victory__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">Victory</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className="victory__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="victory__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="victory__navbar-menu_container scale-up-center">
+          <div className="victory__navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">Victory</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+          <div className="victory__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
+        </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
